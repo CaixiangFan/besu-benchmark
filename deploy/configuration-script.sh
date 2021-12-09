72b34ec9d9b84ab7e814acabe2c70709acdce972
CaixiangFan <caixiang@ualberta.ca>
2021-12-09 14:45:03 -0700

    modified logging level to DEBUG in deploy

--- deploy/configuration-script.sh
+++ deploy/configuration-script.sh
@@ -3,6 +3,6 @@ rm /etc/machine-id
 systemd-machine-id-setup
 systemd-resolve --flush-caches
 cd /home/ubuntu || exit
-sudo -H -u ubuntu bash -c 'git clone https://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@github.com/CaixiangFan/bpet.git'
+sudo -H -u ubuntu bash -c 'git clone https://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@github.com/CaixiangFan/bpet.git'
 cd bpet/deploy || exit
 sudo -H -u ubuntu bash -c 'python3 deploy.py 192.168.226.163 50'
