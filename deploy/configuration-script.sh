e6e6be41a3874f8cd16758ebc895e6ecd7e0ee16
CaixiangFan <caixiang@ualberta.ca>
2021-12-09 15:40:52 -0700

    update configiguration-script.sh

--- deploy/configuration-script.sh
+++ deploy/configuration-script.sh
@@ -3,6 +3,6 @@ rm /etc/machine-id
 systemd-machine-id-setup
 systemd-resolve --flush-caches
 cd /home/ubuntu || exit
-sudo -H -u ubuntu bash -c 'git clone https://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@github.com/CaixiangFan/bpet.git'
+sudo -H -u ubuntu bash -c 'git clone https://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@github.com/CaixiangFan/bpet.git'
 cd bpet/deploy || exit
-sudo -H -u ubuntu bash -c 'python3 deploy.py 192.168.226.163 50'
+sudo -H -u ubuntu bash -c 'python3 deploy.py 192.168.226.163 4'
