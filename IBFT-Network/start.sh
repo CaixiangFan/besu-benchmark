08046038dc59324119f0ca110813926c53ae9cfa
CaixiangFan <caixiang@ualberta.ca>
2021-12-16 17:13:33 -0700

    updated start.sh

--- IBFT-Network/start.sh
+++ IBFT-Network/start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 docker run -d --rm \
     --name $1 \
-    --net host \
     -v ${PWD}/data:/opt/besu/data \
-    -v ${PWD}/genesis.json:/config/genesis.json \
+    -v ${PWD}/../genesis.json:/config/genesis.json \
     -v ${PWD}/config.toml:/config/config.toml \
     hyperledger/besu:21.10 \
     --config-file=/config/config.toml
