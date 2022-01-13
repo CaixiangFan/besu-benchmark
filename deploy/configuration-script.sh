3f649793345bc696b3542801ab183af0e17cf14c
CaixiangFan <caixiang@ualberta.ca>
2022-01-12 22:24:14 -0700

    updated deploy scripts

--- deploy/configuration-script.sh
+++ deploy/configuration-script.sh
@@ -5,4 +5,4 @@ systemd-resolve --flush-caches
 cd /home/ubuntu || exit
 sudo -H -u ubuntu bash -c 'git clone https://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@github.com/CaixiangFan/bpet.git'
 cd bpet/deploy || exit
-sudo -H -u ubuntu bash -c 'python3 deploy.py 10.2.1.9 4'
+sudo -H -u ubuntu bash -c 'python3 deploy.py 192.168.226.176 4'
