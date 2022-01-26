195f0e42800ee738b5c011b87ac9996219522f1e
CaixiangFan <caixiang@ualberta.ca>
2022-01-26 15:32:44 -0700

    update watchdog for cybera deployment

--- deploy/configuration-script.sh
+++ deploy/configuration-script.sh
@@ -7,14 +7,14 @@ cd /home/ubuntu || exit
 sudo apt update -y
 sudo apt install ntpdate -y
 # add the NTP server’s IP address and hostname in the /etc/hosts file
-echo '192.168.226.176	  besu-watchdog' | sudo tee -a /etc/hosts
+echo '10.2.1.9	  watchdog' | sudo tee -a /etc/hosts
 sudo timedatectl set-ntp off
 # install NTP service on the client system
 sudo apt install ntp -y
 # Configure NTP Client
-echo 'server besu-watchdog prefer iburst' | sudo tee -a /etc/ntp.conf
+echo 'server watchdog prefer iburst' | sudo tee -a /etc/ntp.conf
 sudo systemctl restart ntp
 
 sudo -H -u ubuntu bash -c 'git clone https://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@github.com/CaixiangFan/bpet.git'
 cd bpet/deploy || exit
-sudo -H -u ubuntu bash -c 'python3 deploy.py 192.168.226.176 8'
+sudo -H -u ubuntu bash -c 'python3 deploy.py 10.2.1.9 4'
