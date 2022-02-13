f9bc362b3a24856bf1c41a44b05ed9d48829e6e6
CaixiangFan <caixiang@ualberta.ca>
2022-02-12 22:31:03 -0700

    refactored analysis code

--- deploy/configuration-script.sh
+++ deploy/configuration-script.sh
@@ -4,8 +4,6 @@ systemd-machine-id-setup
 systemd-resolve --flush-caches
 cd /home/ubuntu || exit
 
-watchdog = 10.2.1.9
-netsize = 4
 # enable docker remote API 
 sudo sed -i '/ExecStart/s/$/ -H=tcp:\/\/0.0.0.0:2375/' /lib/systemd/system/docker.service
 sudo systemctl daemon-reload
@@ -16,7 +14,7 @@ sudo apt install iftop
 sudo apt update -y
 sudo apt install ntpdate -y
 # add the NTP server’s IP address and hostname in the /etc/hosts file
-echo '$watchdog 	  watchdog' | sudo tee -a /etc/hosts
+echo '192.168.226.176	  watchdog' | sudo tee -a /etc/hosts
 sudo timedatectl set-ntp off
 # install NTP service on the client system
 sudo apt install ntp -y
@@ -26,4 +24,4 @@ sudo systemctl restart ntp
 
 sudo -H -u ubuntu bash -c 'git clone https://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@github.com/CaixiangFan/bpet.git'
 cd bpet/deploy || exit
-sudo -H -u ubuntu bash -c 'python3 deploy.py $watchdog $netsize'
+sudo -H -u ubuntu bash -c 'python3 deploy.py 192.168.226.176 4'
