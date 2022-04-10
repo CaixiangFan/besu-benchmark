e882daef21f9e9eed6a638e15af43d3659e8df3a
caixiang <caixiang@ualberta.ca>
2022-04-10 22:45:26 +0000

    test default xmx

--- deploy/start.sh
+++ deploy/start.sh
@@ -8,7 +8,7 @@ docker run -d --rm \
     -v ${PWD}/data:/opt/besu/data \
     -v ${PWD}/genesis.json:/config/genesis.json \
     -v ${PWD}/config.toml:/config/config.toml \
-    -e BESU_OPTS='-XX:MaxRAMPercentage=80.0' \
+    # -e BESU_OPTS='-XX:MaxRAMPercentagevfcx=80.0 -XshowSettings:vm' \
     hyperledger/besu:21.10 \
     --config-file=/config/config.toml
 
