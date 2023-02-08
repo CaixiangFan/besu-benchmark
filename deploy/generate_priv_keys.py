3bea9196e954e6562ce91e88a82e8c179b3097a7
caixiangfan <caixiang@ualberta.ca>
2023-02-08 17:55:29 +0000

    added scripts to generate prikeys

--- deploy/generate_priv_keys.py
+++ deploy/generate_priv_keys.py
@@ -1,4 +1,11 @@
 import secrets
+import json
+
+# priv_keys = []
+# with open('ibftConfigFile-template.json') as file_read:
+#     data = json.load(file_read)
+#     print(data['genesis']['alloc'])
+
 for i in range(0, 10):
     priv_key = secrets.token_hex(32)
     print(i+1, ': ', priv_key)
